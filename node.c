7502aa329539ac4eafa9bef488c37efec327cf10
Bry <44911576+Bryl33t@users.noreply.github.com>
2020-09-26 15:06:02 +0200

    Create node.c

---
+++ node.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "node.h"
+
+Node *init_list(int value)
+{
+  Node *node = malloc(sizeof(Node));
+  node->value = value;
+  node->previous = NULL;
+  node->next = NULL;
+  return node;
+}
+
+void insert_element(Node *last, int value)
+{
+  while(last->next)
+    last = last->next;
+
+  last->next = malloc(sizeof(Node));
+  last->next->previous = last;
+  last->next->value = value;
+  last->next->next = NULL;
+}
+
+void print_list(Node *node)
+{
+  do {
+    printf("%d\n", node->value);
+    node = node->next;
+  }while(node);
+}
